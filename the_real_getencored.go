4fc3abe98c2ba65b879c759a6f86238c6a1fea19
getencored <38971173+getencored@users.noreply.github.com>
2021-10-12 17:25:09 +0100

    Add files via upload

---
+++ the_real_getencored.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World, I am Ironman.")
+    fmt.Println("My name is Peter Abia")
+    fmt.Println("My email is peterabia7@gmail.com")
+    fmt.Println("Discord username is @getencored")
+    fmt.Println("My Biostack is drug discovery")
+    fmt.Println("While my stack username just in case is @getencored..")
+}
